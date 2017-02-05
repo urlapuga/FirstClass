aad4dbc01b1cc5f64671cf1fffb205aafa7c2ac7
urlapuga <urlapuga@gmail.com>
2017-02-05 16:51:43 +0200

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 05.02.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
