851eda31ef617957793d2ad377d8db504549a524
urlapuga <urlapuga@gmail.com>
2017-02-05 16:59:46 +0200

    Commiting

---
+++ src/FourthClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 05.02.2017.
+ */
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой четвертый класс на Java");
+    }
+}
