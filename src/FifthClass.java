a6945856ef512cb6bc4fe4e7f49ffbc0141dfd43
urlapuga <urlapuga@gmail.com>
2017-02-05 16:59:36 +0200

    Commiting

---
+++ src/FifthClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 05.02.2017.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой пятый класс на Java");
+    }
+}
